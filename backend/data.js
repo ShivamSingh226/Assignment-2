232bc693e1c576f6c0e50114021fd3989cd24597
ShivamSingh226 <shivamsingh226@gmail.com>
2023-07-15 18:12:38 +0530

    Added some new features

--- backend/data.js
+++ backend/data.js
@@ -1,6 +1,10 @@
 const data = {
   products: [
     {
+      _id:'1',
+
+
+
       name: 'Nike Slim Shirt',
       slug: 'nike-slim-shirt',
       category: 'Shirts',
@@ -13,6 +17,7 @@ const data = {
       description: 'high quality shirt',
     },
     {
+      _id:'2',
       name: 'Adidas Fit Shirt',
       slug: 'adidas-fit-shirt',
       category: 'Shirts',
@@ -25,6 +30,7 @@ const data = {
       description: 'high quality shirt',
     },
     {
+      _id:'3',
       name: 'Nike Fit Pant',
       slug: 'nike-fit-pant',
       category: 'Pants',
@@ -37,6 +43,7 @@ const data = {
       description: 'high quality product',
     },
     {
+      _id:'4',
       name: 'Adidas Fit Pant',
       slug: 'adidas-fit-pant',
       category: 'Pants',
