642982e5d47facfa978e6ebe8f6863052e50c25c
ShivamSingh226 <shivamsingh226@gmail.com>
2023-07-16 13:57:57 +0530

    Created a signin page

--- supermarket/src/App.js
+++ supermarket/src/App.js
@@ -9,6 +9,7 @@ import Nav from 'react-bootstrap/Nav';
 import { useContext } from 'react';
 import { Store } from './Store';
 import CartScreen from './screens/CartScreen';
+import SignInScreen from './screens/SignInScreen';
 function App() {
   const { state } = useContext(Store);
   const { cart } = state;
@@ -41,6 +42,7 @@ function App() {
               <Route path="/product/:slug" element={<ProductScreen />} />
               <Route path="/" element={<HomeScreen />} />
               <Route path="/cart" element={<CartScreen/>}/>
+              <Route path="/signin" element={<SignInScreen/>}/>
             </Routes>
           </Container>
         </main>
