2338c075a8ba45392b8b0271dfc75c943b67574f
ShivamSingh226 <shivamsingh226@gmail.com>
2023-07-22 17:09:07 +0530

    deployment of site

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import data from './data.js';
+import path from 'path'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 import seedRouter from './routes/seedRoutes.js';
@@ -19,6 +19,11 @@ app.use('/api/seed',seedRouter)
 app.use('/api/products',productRouter)
 app.use('/api/users',userRouter)
 app.use('/api/orders',orderRouter)
+const __dirname=path.resolve();
+app.use(express.static(path.join(__dirname,'/frontend/build')))
+app.get('*',(req,res)=>{
+  res.send(path.join(__dirname,'/frontend/build/index.html'))
+})
 app.use((err,req,res,next)=>{
   res.status(500).send({message:err.message})
 })
