96f5efdef4ceb4970de37606327f30bff425192d
ShivamSingh226 <shivamsingh226@gmail.com>
2023-07-23 22:17:44 +0530

    Changed some settings

--- backend/server.js
+++ backend/server.js
@@ -20,9 +20,9 @@ app.use('/api/products',productRouter)
 app.use('/api/users',userRouter)
 app.use('/api/orders',orderRouter)
 const __dirname=path.resolve();
-app.use(express.static(path.join(__dirname,'/frontend/build')))
+app.use(express.static(path.join(__dirname,'/supermarket/build')))
 app.get('*',(req,res)=>{
-  res.send(path.join(__dirname,'/frontend/build/index.html'))
+  res.send(path.join(__dirname,'/supermarket/build/index.html'))
 })
 app.use((err,req,res,next)=>{
   res.status(500).send({message:err.message})
